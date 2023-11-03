1ca3197d9793d600888240f28f95146e49b76ec2
wang-annabel <149740503+wang-annabel@users.noreply.github.com>
2023-11-02 19:35:43 -0700

    Annabel - commit

--- git_workshop.py
+++ git_workshop.py
@@ -4,16 +4,17 @@ import math
 # rename your function
 def func1(num_a):
     # placeholder return, replace with your own function
-    return -1
+    return num_a * 2
 
 def func2(num_b):
     # placeholder return, replace with your own function
-    return -1
+    return num_b / 2
 
 # both people should modify this function
 def func3(num_a, num_b):
+    out = num_a + num_b
     # do something with inputs and print something
-    print("placeholder output")
+    print(out)
 
 a = 1
 b = 1
